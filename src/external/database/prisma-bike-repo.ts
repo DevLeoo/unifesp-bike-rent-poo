354e648c8264d1b9c84044171b4d02c113496ff4
Leonardo Candido <leonardo.leotc@gmail.com>
2023-11-03 10:20:18 -0300

    chore: tarefa de aula

---
+++ src/external/database/prisma-bike-repo.ts
@@ -0,0 +1,42 @@
+import { Bike } from "../../bike";
+import { BikeRepo } from "../../ports/bike-repo";
+import prisma from "./db";
+
+export class PrismaBikeRepo implements BikeRepo {
+  async find(id: string): Promise<Bike> {
+    return await prisma.bike.findFirst({
+      where: { id },
+    });
+  }
+
+  async add(bike: Bike): Promise<string> {
+    const addedBike = await prisma.bike.create({
+      data: { ...bike },
+    });
+
+    return addedBike.id;
+  }
+
+  async remove(id: string): Promise<void> {
+    await prisma.user.delete({
+      where: { id },
+    });
+  }
+
+  async update(id: string, bike: Bike): Promise<void> {
+    const updatedBike = await prisma.bike.update({
+      where: {
+        id,
+      },
+      data: {
+        ...bike,
+      },
+    });
+
+    return updatedBike.id;
+  }
+
+  async list(): Promise<Bike[]> {
+    return await prisma.user.findMany({});
+  }
+}
